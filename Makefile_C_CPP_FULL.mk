1c43cf57fb506ecf04e34aa8cfc1935b8cb894e3
Christos Margiolis <christos@christosmarg.xyz>
2020-10-21 04:10:37 +0300

    made POSIX

--- Makefile_C_CPP_FULL.mk
+++ Makefile_C_CPP_FULL.mk
@@ -1,4 +1,6 @@
 # See LICENSE file for copyright and license details.
+# <++>
+.POSIX:
 
 BIN = <++>
 VERSION = <++>
@@ -12,9 +14,11 @@ SRC_DIR = src
 OBJ_DIR = obj
 BIN_DIR = bin
 
-EXT = <++>
-SRC = ${wildcard ${SRC_DIR}/*.${EXT}}
-OBJ = ${SRC:${SRC_DIR}/%.${EXT}=${OBJ_DIR}/%.o}
+#EXT = <++>
+#SRC = ${wildcard ${SRC_DIR}/*.${EXT}}
+#OBJ = ${SRC:${SRC_DIR}/%.${EXT}=${OBJ_DIR}/%.o}
+SRC = <++>
+OBJ = <++>
 
 CP = cp -f
 RM = rm -f
@@ -34,44 +38,44 @@ LDFLAGS = -Llib <++>
 all: options ${BIN}
 
 options:
-		@echo ${BIN} build options:
-		@echo "CFLAGS   = ${CFLAGS}"
-		@echo "LDFLAGS  = ${LDFLAGS}"
-		@echo "CC       = ${CC}"
+	@echo ${BIN} build options:
+	@echo "CFLAGS   = ${CFLAGS}"
+	@echo "LDFLAGS  = ${LDFLAGS}"
+	@echo "CC       = ${CC}"
 
 ${BIN}: ${OBJ}
-		${MKDIR} ${BIN_DIR}
-		${CC} ${LDFLAGS} $^ ${LDLIBS} -o $@
-		${MV} ${BIN} ${BIN_DIR}
+	${MKDIR} ${BIN_DIR}
+	${CC} ${LDFLAGS} ${OBJ} ${LDLIBS} -o $@
+	${MV} ${BIN} ${BIN_DIR}
 
 ${OBJ}: ${SRC}
-		${MKDIR} ${OBJ_DIR}
-		${CC} ${CFLAGS} -c $< -o $@
+	${MKDIR} ${OBJ_DIR}
+	${CC} ${CFLAGS} -c ${SRC} -o $@
 
 dist: clean
-		${MKDIR} ${DIST}
-		${CP} -R <++> ${DIST}
-		${TAR} ${DIST}.tar ${DIST}
-		${GZIP} ${DIST}.tar
-		${RM_DIR} ${DIST}
+	${MKDIR} ${DIST}
+	${CP} -R <++> ${DIST}
+	${TAR} ${DIST}.tar ${DIST}
+	${GZIP} ${DIST}.tar
+	${RM_DIR} ${DIST}
 
 run:
-		./${BIN_DIR}/${BIN}
+	./${BIN_DIR}/${BIN}
 
 install: all
-		${MKDIR} ${DESTDIR}${BIN_DIR} ${DESTDIR}${MAN_DIR}
-		${CP} ${BIN_DIR}/${BIN} ${BIN_DIR}
-		${CP} ${MAN1} ${DESTDIR}${MAN_DIR}
-		sed "s/VERSION/${VERSION}/g" < ${MAN1} > ${DESTDIR}${MAN_DIR}/${MAN1}
-		chmod 755 ${DESTDIR}${BIN_DIR}/${BIN}
-		chmod 644 ${DESTDIR}${MAN_DIR}/${MAN1}
+	${MKDIR} ${DESTDIR}${BIN_DIR} ${DESTDIR}${MAN_DIR}
+	${CP} ${BIN_DIR}/${BIN} ${BIN_DIR}
+	${CP} ${MAN1} ${DESTDIR}${MAN_DIR}
+	sed "s/VERSION/${VERSION}/g" < ${MAN1} > ${DESTDIR}${MAN_DIR}/${MAN1}
+	chmod 755 ${DESTDIR}${BIN_DIR}/${BIN}
+	chmod 644 ${DESTDIR}${MAN_DIR}/${MAN1}
 
 uninstall:
-		${RM} ${DESTDIR}${BIN_DIR}/${BIN}
-		${RM} ${DESTDIR}${MAN_DIR}/${MAN1}
+	${RM} ${DESTDIR}${BIN_DIR}/${BIN}
+	${RM} ${DESTDIR}${MAN_DIR}/${MAN1}
 	
 clean:
-		${RM_DIR} ${BIN_DIR} ${OBJ_DIR}
-		${RM} ${DIST}.tar.gz
+	${RM_DIR} ${BIN_DIR} ${OBJ_DIR}
+	${RM} ${DIST}.tar.gz
 
 .PHONY: all options clean dist install uninstall run
